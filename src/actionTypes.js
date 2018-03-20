16dd3bd14134adfb148b0f49e75db1a659da43a2
Kota Nonaka <kota.kaicho@gmail.com>
2018-03-20 14:23:04 +0900

    reducerを追加

---
+++ src/actionTypes.js
@@ -0,0 +1,5 @@
+export default {
+  // User関連の操作
+  SET_USER: 'SET_USER',
+  UNSET_USER: 'UNSET_USER'
+}
--- src/reducers.js
+++ src/reducers.js
@@ -1,8 +1,20 @@
 // 各Reducerをまとめる
 import {combineReducers} from 'redux'
+import actionTypes from './actionTypes'
 
 const user = (state={}, action) => {
-
+  switch (action.type) {
+    case actionTypes.SET_USER:
+      return Object.assign({}, state, {
+        id: action.payload.id
+      })
+    
+    case actionTypes.UNSET_USER:
+      return {}
+    
+      default:
+        return state
+  }
 }
 
 const rootReducer = combineReducers(user)
