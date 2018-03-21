23f74031510d541c2cdbb91b01a6fe691c6da71a
Kota Nonaka <kota.kaicho@gmail.com>
2018-03-21 13:38:25 +0900

    react-router-redux導入

--- src/store.js
+++ src/store.js
@@ -1,12 +1,16 @@
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
+import {routerMiddleware} from 'react-router-redux'
+import {history} from './router'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(
+    applyMiddleware(thunk, routerMiddleware(history))
+  )
 )
 
 export default store
