3298f1538e86204463d190bc7fab8cb5bcca6a69
Tsubasa <sinnyo_tsubasa@icloud.com>
2018-03-21 11:40:56 +0900

    fix style-loginPage

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -4,6 +4,10 @@ import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 import {requestLogin} from '../../actions/userActions'
 
+// import style
+import '../../styles/main.css'
+import { styles } from "../../styles/style";
+
 class Login extends Component {
   constructor(props) {
     super(props)
@@ -21,31 +25,45 @@ class Login extends Component {
 
   render() {
     return (
-      <Form>
-        <Form.Field>
-          <label>Email Address</label>
-          <input
-            type="email"
-            placeholder="mail@example.com"
-            value={this.state.draftEmail}
-            onChange={(e) => this.setState({draftEmail: e.target.value})}
-          />
-        </Form.Field>
-        <Form.Field>
-          <label>Password</label>
-          <input
-            type="password"
-            placeholder="input your password"
-            value={this.state.draftPassword}
-            onChange={(e) => this.setState({draftPassword: e.target.value})}
-          />
-        </Form.Field>
-        <Button type="submit" onClick={this.onSubmit}>Login</Button>
-      </Form>
+      <div className="fullDiv" >
+        <div className="imageBox registerImage" />
+        <div className="formDiv" >
+          <Form>
+          <div className="top1">
+          <Form.Field>
+            <label>Email Address</label>
+            <input
+              type="email"
+              placeholder="mail@example.com"
+              value={this.state.draftEmail}
+              onChange={(e) => this.setState({draftEmail: e.target.value})}
+              />
+          </Form.Field>
+          <Form.Field>
+            <label>Password</label>
+            <input
+              type="password"
+              placeholder="input your password"
+              value={this.state.draftPassword}
+              onChange={(e) => this.setState({draftPassword: e.target.value})}
+              />
+          </Form.Field>
+          </div>
+          <div className="top2" >
+            <Button style={styles.button} type="submit" onClick={this.onSubmit}>ログイン</Button>
+            <Button style={styles.button} onClick={() => linkTo('/')}>TOP</Button>
+          </div>
+          </Form>
+        </div>
+      </div>
     )
   }
 }
 
 const mapDispatchToProps = dispatch => bindActionCreators({requestLogin},dispatch)
+
+function linkTo(path) {
+  window.location.href = path
+}
 
 export default connect(null, mapDispatchToProps)(Login)
