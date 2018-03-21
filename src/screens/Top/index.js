19afa7fd70f35be7be3c7d7edadb2ad8f96488f0
Tsubasa <sinnyo_tsubasa@icloud.com>
2018-03-21 10:25:47 +0900

    fix topPage link

--- src/screens/Top/index.js
+++ src/screens/Top/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button, Segment } from 'semantic-ui-react'
 import '../../styles/main.css'
+import { styles } from "../../styles/style";
 
 
 export default () => {
@@ -8,10 +9,18 @@ export default () => {
     <div className="fullDiv" >
       <div className="mainImage" />
       <div className="mainDiv" >
-        <p>this is top page!</p>
-        <Button >Login</Button>
-        <Button >Register</Button>
+        <div className="top1" >
+          <p>this is top page!</p>
+        </div>
+        <div className="top2" >
+          <Button onClick={() => linkTo('/login')} style={styles.button} >ログイン</Button>
+          <Button onClick={() => linkTo('/register')} style={styles.button} >登録</Button>
+        </div>
       </div>
     </div>
   )
+}
+
+function linkTo(path) {
+  window.location.href = path
 }
