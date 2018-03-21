c638a00c7b57322c2a8d685133dfd7798088980b
Tsubasa <sinnyo_tsubasa@icloud.com>
2018-03-21 16:07:01 +0900

    fix header-component

--- src/utils/header.js
+++ src/utils/header.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import { Button, Segment } from 'semantic-ui-react'
 import store from "../store";
-import {  } from "../actions/userActions";
+import { requestLogout } from "../actions/userActions";
 export function header(value) {
-  if (value !== null) {
+  if (value) {
     return (
       <div className="header" >
         <p>ログイン中</p>
         <div className="logout" >
-          <Button  >ログアウト</Button>
+          <Button onClick={requestLogout()} >ログアウト</Button>
         </div>
         
       </div>
     )
-  }else (
-    <div className="header" />
-  )
+  }else {
+    return (
+      <div className="header">
+      </div>
+    )
+  }
 }
