ad62aa871b3d46a8c36786254fddf4ae8742253e
Tsubasa <sinnyo_tsubasa@icloud.com>
2018-03-21 15:45:53 +0900

    resolve conflict

--- src/screens/Post/index.js
+++ src/screens/Post/index.js
@@ -11,39 +11,9 @@ class Post extends Component {
 
   render() {
     return (
-<<<<<<< Updated upstream
       <div>
         <h2>{this.props.post.title}</h2>
         <img src={this.props.post.image_path} />
-=======
-      <div className="fullDiv" >
-        { header(this.props.user.email) }
-        <div className="postImageDiv center">
-          <img className="postImage" src={require("../../images/postImage.png")} />
-        </div>
-        <div className="postDiv" >
-          <p>Logined as {this.props.user.email || 'guest'}</p>
-          <Form>
-            <Form.Field>
-              <label>Title</label>
-              <input
-                type="text"
-                placeholder="your photo title"
-                onChange={e => this.setState({draftTitle: e.target.value})}
-              />
-            </Form.Field>
-            <Form.Field>
-              <label>File</label> 
-              <input type="file" name="photo" />
-            </Form.Field>
-            <Button
-              style={styles.button}
-              type="submit"
-              onClick={this.onSubmit}
-            >送信</Button>
-          </Form>
-        </div>
->>>>>>> Stashed changes
       </div>
     )
   }
