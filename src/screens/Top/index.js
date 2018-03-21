dd0fd9fa5fbe1f2e94f028998063f4168650f662
Tsubasa <sinnyo_tsubasa@icloud.com>
2018-03-21 09:53:06 +0900

    CSSを追加

--- src/screens/Top/index.js
+++ src/screens/Top/index.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Button, Segment } from 'semantic-ui-react'
+import '../../styles/main.css'
+
 
 export default () => {
   return (
-    <p>this is top page!</p>
+    <div className="fullDiv" >
+      <div className="mainImage" />
+      <div className="mainDiv" >
+        <p>this is top page!</p>
+        <Button >Login</Button>
+        <Button >Register</Button>
+      </div>
+    </div>
   )
 }
